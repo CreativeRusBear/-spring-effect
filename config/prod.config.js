570804bd5189f9b99f2bf53eda2710523baf2eda
Artem Gusev <gusev2014russia@mail.ru>
2020-11-27 15:15:40 +0300

    updates webpack config for production mode

--- config/prod.config.js
+++ config/prod.config.js
@@ -22,7 +22,7 @@ module.exports = merge(baseConfig, {
     optimization: {
         minimize: true,
         runtimeChunk: {
-            name: 'runtime'
+            name: 'single'
         }
     },
     performance: {
